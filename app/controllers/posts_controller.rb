9e662c1735b16aad32a60f107c7fff70d292405d
mohamedhamdy746 <mohamedhamdy0702@gmail.com>
2026-06-06 16:16:32 +0300

    Implement CRUD actions for posts in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,54 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      # Post, Redirect, Get
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /posts/:id/edit
+  def edit 
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH /posts/:id
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
